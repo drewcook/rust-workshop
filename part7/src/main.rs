ef14bb3bce27c4343da7c8d5783802f040967f5c
Drew Cook <drewcook29@gmail.com>
2022-06-11 20:17:49 -0600

    chore: Lesson 6 solution

--- part7/src/main.rs
+++ part7/src/main.rs
@@ -1,35 +1,32 @@
 fn main() {
     let numbers = vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
+    let other_numbers = vec![1, 2, 3, 4, 5, 6];
+    let (slice1, slice2) = first_three(&numbers, &other_numbers);
 
     // 👉 TODO call sum(), product(), and average() to replace the `= 0` values
     // here, without using .clone() or changing what those functions return.
     //
     // 💡 TIP: You can do this by accepting a slice type - e.g. &[i64]
-    let sum_of_nums = 0;
-    let product_of_nums = 0;
-    let average_of_nums = 0;
-
+    let sum_of_nums = sum(&numbers);
+    let product_of_nums = product(&numbers);
+    let average_of_nums = average(&numbers);
     println!("Sum of these numbers: {}", sum_of_nums);
     println!("Product of these numbers: {}", product_of_nums);
     println!("Average of these numbers: {}", average_of_nums);
 
-    let other_numbers = vec![1, 2, 3, 4, 5, 6];
-    let (slice1, slice2) = first_three(numbers, other_numbers);
-
+    // Print out our slices
     println!("The first three elements in `slice1` are:");
-
     for num in slice1 {
         println!("• {}", num);
     }
 
     println!("The first three elements in `slice2` are:");
-
     for num in slice2 {
         println!("• {}", num);
     }
 }
 
-fn sum(numbers: Vec<i64>) -> i64 {
+fn sum(numbers: &Vec<i64>) -> i64 {
     let mut total = 0;
 
     for num in numbers.iter() {
@@ -39,7 +36,7 @@ fn sum(numbers: Vec<i64>) -> i64 {
     total
 }
 
-fn product(numbers: Vec<i64>) -> i64 {
+fn product(numbers: &Vec<i64>) -> i64 {
     let mut total = 1;
 
     for num in numbers.iter() {
@@ -49,13 +46,16 @@ fn product(numbers: Vec<i64>) -> i64 {
     total
 }
 
-fn average(numbers: Vec<i64>) -> i64 {
+fn average(numbers: &Vec<i64>) -> i64 {
     let length = numbers.len() as i64;
 
     sum(numbers) / length
 }
 
-fn first_three(numbers1: Vec<i64>, numbers2: Vec<i64>) -> (Vec<i64>, Vec<i64>) {
+// Using lifetime annotations so we can return slices from borrowed values
+// Using just <'a> is saying, all things will have the same lifetime here, but it is more constrictive.
+// Using <'a, 'b> actually allows more flexibility and can be less error prone.
+fn first_three<'a, 'b>(numbers1: &'a Vec<i64>, numbers2: &'b Vec<i64>) -> (&'a [i64], &'b [i64]) {
     // 👉 TODO Return two slices, one containing the first 3 elements in
     //    numbers1, and the other containing the first 3 elements in numbers2.
     //
@@ -76,5 +76,7 @@ fn first_three(numbers1: Vec<i64>, numbers2: Vec<i64>) -> (Vec<i64>, Vec<i64>) {
     //        fn first_three<'a, 'b>
     //
     //        fn first_three<'a, 'b, 'c>
-    (numbers1, numbers2)
+    let slice1 = &numbers1[0..3];
+    let slice2 = &numbers2[0..3];
+    (slice1, slice2)
 }
